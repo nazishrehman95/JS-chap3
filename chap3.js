7703fa1c3f618a86e48ead50d9d1e6ac3f400b77
nazishrehman95 <156924657+nazishrehman95@users.noreply.github.com>
2024-05-19 00:05:44 +0500

    Add files via upload

---
+++ chap3.js
@@ -0,0 +1,20 @@
+//Question#1
+let age=15;
+alert("I am " +age+" years old ");
+
+//Question#2
+let visits=14;
+alert("You have visits this site " +visits+ " times")
+
+// Question #3
+let birthyear = 1990;
+document.write("My birthday year is " + birthyear + " <br> Data type of my declared variable is number");
+
+// Add a line break between Question #3 and Question #4
+document.write("<br><br>");
+
+// Question #4
+let visitorsName = "John Doe";
+let productTitle = "T-shirts";
+let quantity = 5;
+document.write(visitorsName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store");
